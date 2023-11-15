05fe68ca33fd97d2e7217e849aa5319e765ebed7
Gus Beringer <gusberinger@gmail.com>
2023-11-15 00:37:01 -0600

    fix dynamic imports

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
     <main>
         <button
-            @click="$router.push('series/234')"
+            @click="$router.push('series/tt0141842')"
             class="p-4 rounded-full bg-green-400 text-white hover:scale-105 transition-all"
         >
             Go To Series
--- src/views/SeriesView.vue
+++ src/views/SeriesView.vue
@@ -1,15 +1,49 @@
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+type SeriesDB = {
+    tconst: string
+    titleType: string
+    primaryTitle: string
+    originalTitle: string
+    isAdult: '0' | '1'
+    startYear: number
+    endYear: number | null
+    runtimeMinutes: number
+    genres: string[]
+    episodes: {
+        tconst: string
+        parentTconst: string
+        seasonNumber: number
+        episodeNumber: number
+        averageRating: number
+        numVotes: number
+        titleType: string
+        primaryTitle: string
+        originalTitle: string
+        isAdult: '0' | '1'
+        startYear: number
+        endYear: number | null
+        runtimeMinutes: number
+        genres: string[]
+    }
+}
+const route = useRoute()
+const db = ref<null | SeriesDB>(null)
+
+onMounted(() => {
+    const tconst = route.params.tconst
+    import(`../assets/series_db/${tconst}.json`).then((module) => {
+        db.value = module.default
+    })
+})
+</script>
 <template>
-    <div class="about">
-        <h1>{{ $route.params }}</h1>
+    <div v-if="db" class="flex min-h-screen flex-col items-center justify-center">
+        <!-- {{ db }} -->
+        <h1>{{ db.primaryTitle }} ({{ db.startYear }}–{{ db.endYear }})</h1>
+        <h2 class="max-w-lg">{{ db.episodes }}</h2>
     </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-    .about {
-        min-height: 100vh;
-        display: flex;
-        align-items: center;
-    }
-}
-</style>
+<style></style>
