5d6167852e5ce4f5db8e5af2e0e2386a6f0f15da
Gus Beringer <gusberinger@gmail.com>
2023-11-17 22:24:29 -0600

    switch to dark on app load if enabled

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import { useDark } from '@vueuse/core'
 import { usePrimeVue } from 'primevue/config';
-import { watch } from 'vue';
+import { onMounted } from 'vue';
 const isDark = useDark()
-
 const PrimeVue = usePrimeVue()
+
+onMounted(() => {
+    if (isDark.value) {
+        PrimeVue.changeTheme('lara-light-teal', 'lara-dark-teal', 'lara-theme')
+    }
+})
 
 </script>
 <template>
