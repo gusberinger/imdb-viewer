260960a30960fab4f950dae497fdd27630a2e8ec
Gus Beringer <gusberinger@gmail.com>
2023-11-20 13:41:34 -0600

    removed unused javascript

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['chart.js', 'vue-chartjs']
+                }
+            }
+        }
+    },
     plugins: [
         vue(),
         vueJsx(),
