cd4d678fe26c3039b958f8c495064863dda55856
Gus Beringer <gusberinger@gmail.com>
2023-11-20 01:36:10 -0600

    add colors button

--- src/components/ColorControls.vue
+++ src/components/ColorControls.vue
@@ -1,14 +1,30 @@
 <script lang="ts" setup>
-import { useDisplayOptionsStore } from '@/stores/displayOptionsStore';
-import { storeToRefs } from 'pinia';
-import ColorPicker from 'primevue/colorpicker';
+import { useDisplayOptionsStore } from '@/stores/displayOptionsStore'
+import { storeToRefs } from 'pinia'
+import ColorPicker from 'primevue/colorpicker'
+import { PlusIcon } from '@heroicons/vue/24/outline'
 const { displayOptions } = storeToRefs(useDisplayOptionsStore())
+
+const addColor = () => {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16)
+    displayOptions.value.colors.push(randomColor)
+}
 </script>
 <template>
     <div class="py-1">
         <label for="">Season Color Palette</label>
-        <div class="flex gap-x-1" >
-            <ColorPicker v-for="(color, idx) in displayOptions.colors" :key="idx" v-model="displayOptions.colors[idx]" />
+        <div class="flex gap-x-1">
+            <ColorPicker
+                v-for="(color, idx) in displayOptions.colors"
+                :key="idx"
+                v-model="displayOptions.colors[idx]"
+            />
+            <button
+                class="group flex h-[28px] w-[28px] items-center justify-center rounded-[6px] border-2 border-stone-400 transition-colors"
+                @click="addColor"
+            >
+                <PlusIcon class="h-5 w-5 fill-stone-800" />
+            </button>
         </div>
     </div>
 </template>
