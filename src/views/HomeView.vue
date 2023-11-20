d21ca1d069af0b5b891dfc819c4a4ed24b737f1c
Gus Beringer <gusberinger@gmail.com>
2023-11-20 13:48:15 -0600

    center h1 text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -31,7 +31,7 @@ import ThemeSwitcher from '@/components/ThemeSwitcher.vue'
                 ></path></svg
         ></a>
         <section>
-            <h1 class="text-2xl">IMDb Charts</h1>
+            <h1 class="text-2xl text-center">IMDb Charts</h1>
             <series-search />
         </section>
         <footer class="fixed bottom-0 mt-10 py-6 text-sm text-gray-400">
