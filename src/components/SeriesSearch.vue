3fcd785dff887445fd39d065cbdb15d2edb71d41
Gus Beringer <gusberinger@gmail.com>
2023-11-20 20:27:22 -0600

    use minisearch for series

--- src/components/SeriesSearch.vue
+++ src/components/SeriesSearch.vue
@@ -1,30 +1,38 @@
 <script setup lang="ts">
 import AutoComplete from 'primevue/autocomplete'
 import { onMounted, ref, watch } from 'vue'
+import MiniSearch from 'minisearch'
 
 type Series = { tconst: string; title: string; startYear: string; endYear: string }
 
 const value = ref<string | Series>('')
 const suggestions = ref<Series[]>([])
-const db = ref<null | Series[]>(null)
+
+const miniSearchObj = ref<null | MiniSearch>(null)
 
 const search = (e: { query: string }) => {
-    const results = db.value
-        ?.filter((item) => {
-            return item.title.toLowerCase().startsWith(e.query.toLowerCase())
-        })
-        .map((item) => ({
-            ...item,
-            display: `${item.title} (${item.startYear}–${item.endYear.replace('\\N', '')})`
-        }))
-        .slice(0, 10)
+    if (miniSearchObj.value === null) return
+    const results = miniSearchObj.value.search(e.query, { prefix: true, fuzzy: 0.2 })
     console.log(results)
-    suggestions.value = results || []
+    suggestions.value = results.map((result) => ({
+        tconst: result.tconst,
+        title: result.title,
+        startYear: result.startYear,
+        endYear: result.endYear,
+        display: `${result.title} (${result.startYear}-${result.endYear})`
+    }))
 }
 
 onMounted(async () => {
     const res = await fetch('/search.json')
-    db.value = await res.json()
+    const json = await res.json()
+    const mini = new MiniSearch({
+        idField: 'tconst',
+        fields: ['title'],
+        storeFields: ['tconst', 'title', 'startYear', 'endYear']
+    })
+    mini.addAll(json)
+    miniSearchObj.value = mini
 })
 
 watch(value, (val) => {
@@ -37,7 +45,7 @@ watch(value, (val) => {
     <div>
         <AutoComplete
             class="rounded-2xl border-2 px-1 py-2"
-            v-if="db !== null"
+            v-if="miniSearchObj !== null"
             v-model="value"
             placeholder="Search Title"
             :suggestions="suggestions"
