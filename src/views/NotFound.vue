a1489da91cec74a0c838096cbd04141c82a6983f
Gus Beringer <gusberinger@gmail.com>
2023-11-23 15:47:33 -0600

    fix content layout shift on 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,9 @@
 <template>
-    <main class="flex h-screen flex-col items-center justify-center gap-y-5">
-        <h1 class="text-5xl">404 Page Not Found</h1>
+    <main class="flex h-screen flex-col items-center justify-center gap-y-5 mx-5 ">
+        <h1 class="text-5xl text-center">404 Page Not Found</h1>
         <img
-            class="w-[90vw]"
+            class="w-[65rem] shadow-md"
+            width="1040"
             src="/vincent.gif"
             alt="John Travolta looking confused, scene from Pulp Fiction."
         />
