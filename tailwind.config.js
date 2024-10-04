b140990a4fce07e017865bbb341c52be3546a3c0
Gus Beringer <gusberinger@gmail.com>
2024-10-04 01:21:31 -0500

    Uninstall tailwind primeui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-    plugins: [require('tailwindcss-primeui')],
     darkMode: ['class'],
     content: [
         './pages/**/*.{ts,tsx,vue}',
