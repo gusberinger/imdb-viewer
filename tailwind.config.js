a275d3e52fbc2ec0f97bbc4af6530c7673a2d959
Gus Beringer <gusberinger@gmail.com>
2024-10-04 00:18:30 -0500

    Add primeui package to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
+    plugins: [require('tailwindcss-primeui')],
     darkMode: ['class'],
     content: [
         './pages/**/*.{ts,tsx,vue}',
