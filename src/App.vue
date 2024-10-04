ad001978f965f49134dba9ad1d5585f965f55ec8
Gus Beringer <gusberinger@gmail.com>
2024-10-04 01:52:05 -0500

    Remove empty script for App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,3 @@
-<script setup lang="ts">
-import { useDark } from '@vueuse/core'
-import { usePrimeVue } from 'primevue/config'
-
-</script>
 <template>
     <RouterView :key="$route.fullPath" />
 </template>
